6a164a2741a59b89bcc5e64f3a3c97183e4b6215
Anthony Cox <motorhead1991@hotmail.com>
2015-05-17 07:59:18 -0600

    Remove "error stop" from java warnings

--- core/main.mk
+++ core/main.mk
@@ -164,7 +164,6 @@ $(info $(space))
 $(info Please follow the machine setup instructions at)
 $(info $(space)$(space)$(space)$(space)https://source.android.com/source/initializing.html)
 $(info ************************************************************)
-$(error stop)
 endif
 
 # Check for the current JDK.
@@ -215,7 +214,6 @@ $(info $(space))
 $(info Please follow the machine setup instructions at)
 $(info $(space)$(space)$(space)$(space)https://source.android.com/source/download.html)
 $(info ************************************************************)
-$(error stop)
 endif
 
 
