264a8f2cbbac9ef1e664f85f5d743288cefa9528
Chet Kener <Cl3Kener@gmail.com>
2015-06-07 22:39:17 -0400

    Add PRODUCT_PACKAGES for Google's prebuilt WebView
    According to https://github.com/DARKPOP/frameworks_webview/blob/dark-5.1/README.mkdn
    
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- target/product/core_minimal.mk
+++ target/product/core_minimal.mk
@@ -73,6 +73,8 @@ PRODUCT_PACKAGES += \
     uncrypt \
     voip-common \
     webview \
+    libwebviewchromium_loader \
+    libwebviewchromium_plat_support \
     wifi-service
 
 PRODUCT_COPY_FILES += \
