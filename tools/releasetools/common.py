a7f8460362a227898eba10040b87494dfcc562ee
Anthony Cox <motorhead1991@hotmail.com>
2015-09-18 23:15:18 -0600

    No more "Patching", Welcome to AreaROM

--- tools/releasetools/common.py
+++ tools/releasetools/common.py
@@ -1048,7 +1048,7 @@ class BlockDifference:
   def WriteScript(self, script, output_zip, progress=None):
     if not self.src:
       # write the output unconditionally
-      script.Print("Patching %s image unconditionally..." % (self.partition,))
+      script.Print("Welcome to AreaROM" % (self.partition,))
     else:
       script.Print("Patching %s image after verification." % (self.partition,))
 
